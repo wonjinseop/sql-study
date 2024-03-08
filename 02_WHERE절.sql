0cb02370d7c562f77f7a8977474ddb398d6e57c6
wonjinseop <wonjinseop7@naver.com>
2024-03-08 17:49:13 +0900

    SELECT 기초 문법 학습

---
+++ 02_WHERE절.sql
@@ -0,0 +1,49 @@
+
+
+SELECT * FROM employees;
+
+-- WHERE절 비교 (데이터 값은 대/소문자를 구분합니다.)
+SELECT first_name, last_name, job_id
+FROM employees
+WHERE job_id = 'IT_PROG';
+
+SELECT *
+FROM employees
+WHERE last_name = 'King';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
