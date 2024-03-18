2105403d186331e8c2bc3288a7a472335e330d70
wonjinseop <wonjinseop7@naver.com>
2024-03-18 11:47:42 +0900

    PL/SQL 제어문

---
+++ PL_SQL/02_PLSQL조건문.sql
@@ -0,0 +1,114 @@
+
+-- IF문
+DECLARE
+    v_salary NUMBER := 0;
+    v_department_id NUMBER := 0;
+BEGIN
+    
+    v_department_id := ROUND(DBMS_RANDOM.VALUE(10, 110), -1);
+    dbms_output.put_line('생성된 난수: ' || v_department_id);
+    
+    SELECT
+        salary
+    INTO
+        v_salary
+    FROM employees
+    WHERE department_id = v_department_id
+    AND ROWNUM = 1; -- 첫째 값만 구해서 변수에 저장.
+    
+    IF
+        v_salary <= 5000
+    THEN
+        dbms_output.put_line('급여가 좀 낮음!');
+    ELSIF
+        v_salary <= 9000
+    THEN
+        dbms_output.put_line('급여가 중간임!');
+    ELSE
+        dbms_output.put_line('급여가 높음!');
+    END IF;
+    
+END;
+
+
+-- CASE문
+DECLARE
+    v_salary NUMBER := 0;
+    v_department_id NUMBER := 0;
+BEGIN
+    
+    v_department_id := ROUND(DBMS_RANDOM.VALUE(10, 110), -1);
+    dbms_output.put_line('생성된 난수: ' || v_department_id);
+    
+    SELECT
+        salary
+    INTO
+        v_salary
+    FROM employees
+    WHERE department_id = v_department_id
+    AND ROWNUM = 1; -- 첫째 값만 구해서 변수에 저장.
+    
+    CASE
+        WHEN v_salary <= 5000 THEN
+            dbms_output.put_line('급여가 좀 낮음!');
+        WHEN v_salary <= 9000 THEN
+            dbms_output.put_line('급여가 중간임!');
+        ELSE
+            dbms_output.put_line('급여가 높음!');
+        END CASE;
+END;
+
+-- 중첩 IF문
+DECLARE
+    v_salary NUMBER := 0;
+    v_department_id NUMBER := 0;
+    v_commission NUMBER := 0;
+BEGIN
+    
+    v_department_id := ROUND(DBMS_RANDOM.VALUE(10, 110), -1);
+    dbms_output.put_line('생성된 난수: ' || v_department_id);
+    
+    SELECT
+        salary, commission_pct
+    INTO
+        v_salary, v_commission
+    FROM employees
+    WHERE department_id = v_department_id
+    AND ROWNUM = 1; -- 첫째 값만 구해서 변수에 저장.
+    
+    IF v_commission > 0 THEN
+        IF v_commission > 0.15 THEN
+            dbms_output.put_line('커미션이 15% 이상입니다!');
+            dbms_output.put_line(v_salary * v_commission);
+        END IF;
+    ELSE
+        dbms_output.put_line('커미션이 없어요 ㅜㅜ');
+    END IF;
+END;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PL_SQL/03_PLSQL반복문_탈출문.sql
@@ -0,0 +1,143 @@
+
+-- WHILE문
+DECLARE
+    v_total NUMBER := 0;
+    v_count NUMBER := 1; -- begin
+BEGIN
+    WHILE v_count <= 10 -- end
+    LOOP
+        v_total := v_total + v_count;
+        v_count := v_count + 1; -- step
+    END LOOP;
+    dbms_output.put_line(v_total);
+END;
+
+-- 탈출문 EXIT
+DECLARE
+    v_total NUMBER := 0;
+    v_count NUMBER := 1; -- begin
+BEGIN
+    WHILE v_count <= 10 -- end
+    LOOP
+        EXIT WHEN v_count = 5; -- break
+    
+        v_total := v_total + v_count;
+        v_count := v_count + 1; -- step
+    END LOOP;
+    dbms_output.put_line(v_total);
+END;
+
+-- FOR문
+DECLARE
+    v_num NUMBER := 7;
+BEGIN
+
+    FOR i IN 1..9 -- .을 두개 작성해서 범위를 표현
+    LOOP
+        dbms_output.put_line(v_num || ' X ' || i || ' = ' || v_num * i);
+    END LOOP;
+
+END;
+
+-- CONTINUE문
+DECLARE
+    v_num NUMBER := 7;
+BEGIN
+
+    FOR i IN 1..9
+    LOOP
+        CONTINUE WHEN MOD(i, 2) = 0;
+        dbms_output.put_line(v_num || ' X ' || i || ' = ' || v_num * i);
+    END LOOP;
+
+END;
+
+--------------------------------------------------------------------------------
+
+-- 1. 모든 구구단을 출력하는 익명 블록을 만드세요. (2 ~ 9단)
+-- 짝수단만 출력해 주세요. (2, 4, 6, 8)
+-- 참고로 오라클 연산자 중에는 나머지를 알아내는 연산자가 없어요. (% 없음~)
+DECLARE
+BEGIN
+    FOR num IN 2..9
+    LOOP
+        IF MOD(num, 2) = 0 THEN
+            dbms_output.put_line('---------- ' || num || '단' || ' ----------');
+            FOR i IN 1..9
+            LOOP
+                dbms_output.put_line(num || ' X ' || i || ' = ' || num * i);
+            END LOOP;
+        END IF;
+    END LOOP;
+END;
+
+DECLARE
+BEGIN
+    FOR num IN 2..9
+    LOOP
+        CONTINUE WHEN MOD(num, 2) = 1;
+        dbms_output.put_line('---------- ' || num || '단' || ' ----------');
+        FOR i IN 1..9
+        LOOP
+            dbms_output.put_line(num || ' X ' || i || ' = ' || num * i);
+        END LOOP;
+    END LOOP;
+END;
+
+-- 2. INSERT를 300번 실행하는 익명 블록을 처리하세요.
+-- board라는 이름의 테이블을 만드세요. (bno, writer, title 컬럼이 존재합니다.)
+-- bno는 SEQUENCE로 올려 주시고, writer와 title에 번호를 붙여서 INSERT 진행해 주세요.
+-- ex) 1, test1, title1 -> 2 test2 title2 -> 3 test3 title3 ....
+CREATE TABLE board (
+    bno NUMBER,
+    writer VARCHAR(30),
+    title VARCHAR(30)
+);
+
+CREATE SEQUENCE b_seq
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 10000
+    NOCYCLE
+    NOCACHE;
+
+DECLARE
+    v_num NUMBER := 1;
+BEGIN
+    WHILE v_num <= 300
+    LOOP
+        INSERT INTO board
+        VALUES(b_seq.NEXTVAL, 'test'||v_num, 'title'||v_num);
+        v_num := v_num + 1;
+    END LOOP;
+END;
+
+SELECT * FROM BOARD
+ORDER BY bno DESC;
+
+COMMIT;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
